aebc7e77e12116f6ac72eda2af02cb6f61cd779a
Willan Shrock <willanshrock@Willans-MacBook-Air-3.local>
2025-04-14 11:29:43 -0500

    updated TDD lab

--- Tests/test_production.py
+++ Tests/test_production.py
@@ -9,7 +9,9 @@ from production import reverse_all_words
 from production import main
 
 class TestReverseWord(unittest.TestCase):
+    ''' Test cases for the reverse_word function '''
     def test_reverse_word(self):
+        ''' Test the reverse_word function '''
         self.assertEqual(reverse_word("hello"), "olleh")
         self.assertEqual(reverse_word("W"), "W")
         self.assertEqual(reverse_word(""), "")
@@ -17,7 +19,9 @@ class TestReverseWord(unittest.TestCase):
         self.assertEqual(reverse_word("hello world"), "dlrow olleh")
 
 class TestReverseAllWords(unittest.TestCase):
+    ''' Test cases for the reverse_all_words function '''
     def test_reverse_all_words(self):
+        ''' Test the reverse_all_words function '''
         self.assertEqual(reverse_all_words("hello world"), "olleh dlrow")
         self.assertEqual(reverse_all_words("W"), "W")
         self.assertEqual(reverse_all_words(""), "")
@@ -25,7 +29,9 @@ class TestReverseAllWords(unittest.TestCase):
 
 
 class TestMainFunction(unittest.TestCase):
+    ''' Test cases for the main function '''
     def test_main_prints_correct_output(self):
+        ''' Test the main function with a sample input '''
         sys.argv = ['production.py', 'this', 'is', 'a', 'test']
         sys.stdout = StringIO()
 
@@ -35,6 +41,7 @@ class TestMainFunction(unittest.TestCase):
         self.assertEqual(output, "siht si a tset")
 
     def test_main_no_args(self):
+        ''' Test the main function with no arguments '''
         sys.argv = ['production.py']  # No phrase provided
         sys.stdout = StringIO()
 
@@ -43,6 +50,5 @@ class TestMainFunction(unittest.TestCase):
         output = sys.stdout.getvalue().strip()
         self.assertEqual(output, "Provide a phrase.")
 
-    
 if __name__ == '__main__':
     unittest.main()
--- production.py
+++ production.py
@@ -4,22 +4,31 @@ A file for the production code
 import sys
 
 def reverse_word(word):
- if word == "":
-     return ""
- elif len(word) == 1:
-    return word
- else: 
-    return word[::-1]
- 
+    '''
+    Function to reverse a word
+    '''
+    if word == "":
+        return ""
+    if len(word) == 1:
+        return word
+    else:
+        return word[::-1]
+
 def reverse_all_words(sentence):
+    '''
+    Function to reverse all words in a sentence
+    '''
     words = sentence.split()
     output = []
     for word in words:
-       current = reverse_word(word)
-       output.append(current)
+        current = reverse_word(word)
+        output.append(current)
     return " ".join(output)
 
 def main():
+    '''
+    Main function to run the script
+    '''
     if len(sys.argv) < 2:
         print("Provide a phrase.")
         return
